130a1b702677aa562d44e1194f0ef6ce1bd0c8a7
pjgowtham <pjgowtham@gmail.com>
2024-06-29 08:40:52 +0530

    oplus-fwk: Add missing classes

--- oplus-fwk/src/android/common/IOplusCommonFeature.java
+++ oplus-fwk/src/android/common/IOplusCommonFeature.java
@@ -2,8 +2,8 @@ package android.common;
 
 import android.common.OplusFeatureList;
 
+/* loaded from: classes.dex */
 public interface IOplusCommonFeature {
-
     default OplusFeatureList.OplusIndex index() {
         return OplusFeatureList.OplusIndex.End;
     }
